7774cbfb356cdab2c1c5ebadf7ba53dc38bdbe58
Herbert J. Bernstein <yayahjb@gmail.com>
2006-07-24 15:30:31 +0000

    CBFlib base 0.1 release by Paul Ellis on 17 April 1998

---
+++ CBFlib/src/cbf_compress.h
@@ -0,0 +1,44 @@
+
+#ifndef CBF_COMPRESS_H
+#define CBF_COMPRESS_H
+
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+#include <stdio.h>
+
+#include "cbf_file.h"
+
+
+  /* Compress an array */
+
+int cbf_compress (void *buf, size_t elsize, int elsign, size_t nelem,
+                             unsigned int compression, size_t repeat,
+                             cbf_file *file);
+
+
+  /* Get the parameters of an array (read up to the start of the table) */
+  
+int cbf_decompress_parameters (int *eltype, int *elsigned, int *elunsigned,
+                               size_t *nelem, int *minelem,
+                               int *maxelem, unsigned int *compression,
+                               cbf_file *file);
+
+
+  /* Decompress an array (from the start of the table) */
+
+int cbf_decompress (void *buf, size_t elsize, int elsign, 
+                               size_t nelem, size_t *nelem_read,
+                               unsigned int compression, cbf_file *file);
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+#endif /* CBF_COMPRESS_H */
---
+++ CBFlib/src/cbf_packed.h
@@ -0,0 +1,36 @@
+
+#ifndef CBF_PACKED_H
+#define CBF_PACKED_H
+
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+#include <stdio.h>
+
+#include "cbf_file.h"
+
+
+  /* Compress an array */
+
+int cbf_compress_packed (void *buf, size_t elsize, int elsign, size_t nelem,
+                         unsigned int compression, size_t repeat,
+                         cbf_file *file);
+
+
+  /* Decompress an array */
+
+int cbf_decompress_packed (void *buf, size_t elsize, int elsign, 
+                           size_t nelem, size_t *nelem_read,
+                           unsigned int compression, cbf_file *file);
+
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+#endif /* CBF_PACKED_H */
