a7d9176a24cc666ecfc353739ee887b4eb442259
Herbert J. Bernstein <yayahjb@gmail.com>
2016-03-13 17:17:01 -0400

    Change to use of pcre for regex for all platforms Introduce CBF_PREFIX for install prefix for CBFlib Allow REGEX_PREFIX to specify a REGEX that is already installed Allow HDF5_PREFEX to specify an HDF5 that is already installed   -- HJB

--- CBFlib_bleeding_edge/m4/setup_py.m4
+++ CBFlib_bleeding_edge/m4/setup_py.m4
@@ -12,7 +12,7 @@ e = Extension(''`_pycbf''`,
               sources = ["pycbf_wrap.c","../src/cbf_simple.c"],
          extra_compile_args=["-g"],
          'm4_ifelse(regexlibdir,`NOREGEXLIBDIR',`library_dirs=["../lib/"],',`library_dirs=["../lib/","'regexlibdir`"],')`
-         'm4_ifelse(regexlib,`NOREGEXLIB',`libraries=["cbf"],',`libraries=["cbf","'regexlib`"],')`
+         'm4_ifelse(regexlib,`',`libraries=["cbf"],',`libraries=["cbf","'regexlib`"],')`
          include_dirs = ["../include","'hdf5_prefix`/include"] )
             
 # Build it
--- CBFlib_bleeding_edge/pycbf/setup.py
+++ CBFlib_bleeding_edge/pycbf/setup.py
@@ -1,3 +1,7 @@
+#
+#  pycbf/setup.py generated from m4/setup_py.m4
+# 
+
 # Import the things to build python binary extensions
 
 from distutils.core import setup, Extension
@@ -5,11 +9,11 @@ from distutils.core import setup, Extension
 # Make our extension module
 
 e = Extension('_pycbf',
-              sources = ["pycbf_wrap.c","../src/cbf_simple.c","../src/cbf_airy_disk.c"],
+              sources = ["pycbf_wrap.c","../src/cbf_simple.c"],
          extra_compile_args=["-g"],
-         library_dirs=["../solib/"],
-         libraries=["cbf"],
-         include_dirs = ["../include"] )
+         library_dirs=["../lib/","/Users/yaya/desktop/ncbf/CBFlib_0.9.5.13_build/CBFlib-0.9.5.13/lib"],
+         libraries=["cbf","pcreposix"],
+         include_dirs = ["../include","/Users/yaya/desktop/ncbf/CBFlib_0.9.5.13_build/CBFlib-0.9.5.13/include"] )
             
 # Build it
-setup(name="_pycbf",ext_modules=[e],py_modules = ['pycbf'])
+setup(name="_pycbf",ext_modules=[e],)
