71387bf2fe7a4d4a7567fed899192850275bc984
Peter Chang <peter.chang@diamond.ac.uk>
2020-12-10 19:06:04 +0000

    Update Java bindings
    Rename swig Java binding file, use nio buffers for faster data access,
    make tests more extensive. Also tweaked Makefiles so HDF5_PREFIX will
    override use of downloaded library

--- examples/testcbf.c
+++ examples/testcbf.c
@@ -1,21 +1,244 @@
+
+#include <stdio.h>
+#include <sys/time.h>
+
 #include "cbf.h"
+#include "cbf_tree.h"
 
-int main() {
-	FILE *f;
-	cbf_handle ch;
-	int status;
-	unsigned int m;
+void readheader(cbf_handle ch, int *dims)
+{
+    int status;
+    const char *arrayid;
+    const char *s;
+    int i, d;
 
-	f = fopen("examples/template_pilatus6m_2463x2527.cbf", "rb");
+    status = cbf_find_column(ch, "array_id");
+    printf("find_col (%d)\n", status);
 
-	cbf_make_handle(&ch);
+    status = cbf_get_value(ch, &arrayid);
+    printf("get_val (%d) = %s\n", status, arrayid);
 
-	status = cbf_read_widefile(ch, f, MSG_DIGEST);
-	printf("read_widefile (%d)\n", status);
+    status = cbf_find_category(ch, "array_structure_list");
+    printf("find_cat (%d)\n", status);
 
-	status = cbf_count_datablocks(ch, &m);
-	printf("count_dbs (%d) = %d\n", status, m);
+    status = cbf_rewind_row(ch);
+    printf("rewind_row (%d)\n", status);
 
-	/*  fclose(f);*/ /* let cbflib handle the closing of a file */
-	return 0;
+    status = cbf_find_column(ch, "array_id");
+    printf("find_col (%d)\n", status);
+
+    /* Attempt to find rows that matches above array_id */
+    while ((status = cbf_find_nextrow(ch, arrayid)) == 0) {
+        printf("find_nrow (%d)\n", status);
+
+        status = cbf_find_column(ch, "index");
+        printf("find_col (%d)\n", status);
+        status = cbf_get_integervalue(ch, &i);
+        printf("get_int (%d) = %d\n", status, i);
+        d = i - 1;
+
+        status = cbf_find_column(ch, "dimension");
+        printf("find_col (%d)\n", status);
+        status = cbf_get_integervalue(ch, &i);
+        printf("get_int (%d) = %d\n", status, i);
+        dims[d] = i;
+
+        status = cbf_find_column(ch, "precedence");
+        printf("find_col (%d)\n", status);
+        status = cbf_get_integervalue(ch, &i);
+        printf("get_int (%d) = %d\n", status, i);
+
+        status = cbf_find_column(ch, "direction");
+        printf("find_col (%d)\n", status);
+        status = cbf_get_value(ch, &s);
+        printf("get_val (%d) = %s\n", status, s);
+
+        status = cbf_find_column(ch, "axis_set_id");
+        printf("find_col (%d)\n", status);
+        status = cbf_get_value(ch, &s);
+        printf("get_val (%d) = %s\n", status, s);
+
+        status = cbf_find_column(ch, "array_id");
+        printf("find_col (%d)\n", status);
+    }
+    printf("find_nrow (%d)\n", status);
+
+    printf("Dimensions: %dx%d\n", dims[0], dims[1]);
+
+    /* Attempt to load data */
+    status = cbf_find_category(ch, "array_data");
+    printf("find_cat (%d)\n", status);
+    status = cbf_find_column(ch, "array_id");
+    printf("find_col (%d)\n", status);
+    status = cbf_find_row(ch, arrayid);
+    printf("find_row (%d)\n", status);
+    status = cbf_find_column(ch, "data");
+    printf("find_col (%d)\n", status);
+}
+
+/*
+ * return time taken in milliseconds
+ */
+int loadfile(const char* name)
+{
+    FILE *f;
+    cbf_handle ch;
+    int status;
+    unsigned int m;
+    const char *dbname;
+    int i;
+    CBF_NODETYPE ntype;
+    struct timeval stv, dtv, ftv;
+    int dims[2];
+    int fullheader = 0;
+
+    size_t dsize;
+    size_t rsize;
+
+    unsigned int cifcomp = 0;
+    int bid = 0, els = 0, elu = 0;
+    int minel = 0, maxel = 0, isre = 0;
+    size_t elsize = 0, elnum = 0;
+    size_t dim1 = 0, dim2 = 0, dim3 = 0, pad = 0;
+    const char *byteorder = NULL;
+
+    int isreal;
+    long dtime;
+    long ttime;
+
+
+    gettimeofday(&stv, NULL);
+    f = fopen(name, "rb");
+
+    cbf_make_handle(&ch);
+
+#if 0
+    {
+      long flen;
+      char *buffer;
+
+      fseek(f, 0, SEEK_END);
+      flen = ftell(f);
+      rewind(f);
+
+      buffer = malloc(flen);
+      fread(buffer, 1, flen, f);
+      fclose(f);
+      status = cbf_read_buffered_file(ch, NULL, MSG_DIGEST|CBF_PARSE_WIDE, buffer, flen);
+/*      printf("read_buffered_file(%d)\n", status); */
+      free(buffer);
+    }
+#else
+    rewind(f);
+
+    status = cbf_read_widefile(ch, f, MSG_DIGEST);
+/*    printf("read_widefile (%d)\n", status); */
+#endif
+
+    status = cbf_rewind_datablock(ch);
+    printf("rewind_db (%d)\n", status);
+
+    status = cbf_count_datablocks(ch, &m);
+    printf("count_dbs (%d) = %d\n", status, m);
+
+    status = cbf_datablock_name(ch, &dbname);
+    printf("db_name (%d) = %s\n", status, dbname);
+
+    status = cbf_rewind_blockitem(ch, &ntype);
+    printf("rewind_bi (%d) = %d\n", status, ntype);
+
+    status = cbf_find_category(ch, "diffrn_data_frame");
+    printf("find_cat (%d)\n", status);
+    if (status != 0) {
+        status = cbf_find_category(ch, "diffrn_frame_data");
+        printf("find_cat (%d)\n", status);
+    }
+
+    if (status == 0) {
+        readheader(ch, dims);
+        fullheader = 1;
+    } else {
+        printf("Mini CBF!\n");
+        status = cbf_find_category(ch, "array_data");
+        printf("find_cat (%d)\n", status);
+        status = cbf_find_column(ch, "data");
+        printf("find_col (%d)\n", status);
+    }
+
+    dsize = dims[0]*dims[1];
+
+    status = cbf_get_arrayparameters_wdims(ch, &cifcomp, &bid, &elsize,
+					       &els, &elu, &elnum,
+					       &minel, &maxel, &isre,
+					       &byteorder, &dim1, &dim2, &dim3, &pad);
+    printf("get_aparams (%d) = %d, %d, %ld, %d, %d,\n", status, cifcomp, bid, (long) elsize, els, elu);
+    printf(" %ld, %d, %d, %d, %s,\n", (long) elnum, minel, maxel, isre, (byteorder == NULL) ? "null" : byteorder);
+    printf(" %ld, %ld, %ld, %ld\n", (long) dim1, (long) dim2, (long) dim3, (long) pad);
+    if (fullheader && dsize != elnum) {
+        printf("Mismatch of CBF binary data size: %ld != %ld\n", (long) dsize, (long) elnum);
+        return 0;
+    }
+    dsize = elnum;
+
+    isreal = (isre == 1);
+
+    gettimeofday(&dtv, NULL);
+    if (isreal) {
+        double *ddata;
+
+        ddata = (double *) malloc(sizeof(double)*dsize);
+        status = cbf_get_realarray(ch, &i, (void *)ddata,
+                   sizeof(double), dsize, &rsize);
+        if (status == 0) {
+            printf("get_dblarray (%d) %ld/%ld\n", status, (long) rsize, (long) dsize);
+            double *darray = (double *) malloc(sizeof(double)*dsize);
+            for (i=0; i<dsize; i++)
+                darray[i] = ddata[i];
+            printf("Sample is %g\n", darray[42]);
+            free(darray);
+        }
+        free(ddata);
+    } else {
+        int *idata;
+
+        idata = (int *) malloc(sizeof(int)*dsize);
+        status = cbf_get_integerarray(ch, &i, (void *)idata,
+                      sizeof(int), els, dsize, &rsize);
+        if (status == 0) {
+            printf("get_intarray (%d) %ld/%ld\n", status, (long) rsize, (long) dsize);
+            int *iarray = (int *) malloc(sizeof(int)*dsize);
+            for (i=0; i<dsize; i++)
+                iarray[i] = idata[i];
+            printf("Sample is %d\n", iarray[42]);
+            free(iarray);
+        }
+        free(idata);
+    }
+
+    cbf_free_handle(ch); /* let cbflib handle the closing of a file */
+
+    gettimeofday(&ftv, NULL);
+    dtime = (ftv.tv_sec - dtv.tv_sec)*1000 + (ftv.tv_usec - dtv.tv_usec)/1000;
+    ttime = (ftv.tv_sec - stv.tv_sec)*1000 + (ftv.tv_usec - stv.tv_usec)/1000;
+    fprintf(stderr, "Time: %dms (%dms)\n", (int) ttime, (int) dtime);
+    return (int) ttime;
+}
+
+int main(int argc, char *argv [])
+{
+    char *name;
+    int n;
+    int t = 0;
+
+    if (argc < 2)
+        name = "examples/template_pilatus6m_2463x2527.cbf";
+    else
+        name = argv[1];
+
+    loadfile(name);
+
+    for (n = 0; n < 10; n++)
+        t += loadfile(name);
+    fprintf(stderr, "Ave: %dms\n", t/10);
+    return 0;
 }
--- examples/testcbf.java
+++ examples/testcbf.java
@@ -1,22 +1,289 @@
+
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import org.iucr.cbflib.*;
 
 // javac -cp cbflib-0.8.0.jar testcbf.java
 // LD_LIBRARY_PATH=solib java -cp cbflib-0.8.0.jar:. testcbf
+/**
+ *
+ */
 public class testcbf {
 
 	static {
-		System.loadLibrary("cbf_wrap");
+		try {
+			System.loadLibrary("libpcre");
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Could not load regex");
+		}
+		try {
+			System.loadLibrary("cbf_wrap");
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Could not load wrapper");
+		}
 	}
 
-	public static void main(String[] args) {
-		SWIGTYPE_p_FILE f = cbf.fopen("examples/template_pilatus6m_2463x2527.cbf", "rb");
-		cbf_handle_struct chs = new cbf_handle_struct();
+	/**
+	 * @param chs
+	 * @param dims
+	 */
+	private static void readheader(cbf_handle_struct chs, int[] dims) {
 		int status = 0;
-		status = cbf.cbf_read_widefile(chs, f, cbfConstants.MSG_DIGEST);
-		System.out.println("read_widefile (" + status + ")");
+		SWIGTYPE_p_p_char s = cbf.new_charPP();
+		intP ip = new intP();
 
-		uintp mp = new uintp();
+		status = cbf.cbf_find_column(chs, "array_id");
+		System.out.println("find_col (" + status + ")");
+
+		status = cbf.cbf_get_value(chs, s);
+		String arrayid = cbf.charPP_value(s);
+		System.out.println("get_val (" + status + ") = " + arrayid);
+
+		status = cbf.cbf_find_category(chs, "array_structure_list");
+		System.out.println("find_cat (" + status + ")");
+
+		status = cbf.cbf_rewind_row(chs);
+		System.out.println("rewind_row (" + status + ")");
+
+		status = cbf.cbf_find_column(chs, "array_id");
+		System.out.println("find_col (" + status + ")");
+
+		int d;
+
+		// Attempt to find rows that matches above array_id
+		while ((status = cbf.cbf_find_nextrow(chs, arrayid)) == 0) {
+			System.out.println("find_nrow (" + status + ")");
+
+			status = cbf.cbf_find_column(chs, "index");
+			System.out.println("find_col (" + status + ")");
+			status = cbf.cbf_get_integervalue(chs, ip.cast());
+			System.out.println("get_int (" + status + ") = " + ip.value());
+			d = ip.value() - 1;
+
+			status = cbf.cbf_find_column(chs, "dimension");
+			System.out.println("find_col (" + status + ")");
+			status = cbf.cbf_get_integervalue(chs, ip.cast());
+			System.out.println("get_int (" + status + ") = " + ip.value());
+			dims[d] = ip.value();
+
+			status = cbf.cbf_find_column(chs, "precedence");
+			System.out.println("find_col (" + status + ")");
+			status = cbf.cbf_get_integervalue(chs, ip.cast());
+			System.out.println("get_int (" + status + ") = " + ip.value());
+
+			status = cbf.cbf_find_column(chs, "direction");
+			System.out.println("find_col (" + status + ")");
+			status = cbf.cbf_get_value(chs, s);
+			System.out.println("get_val (" + status + ") = " + cbf.charPP_value(s));
+
+			status = cbf.cbf_find_column(chs, "axis_set_id");
+			System.out.println("find_col (" + status + ")");
+			status = cbf.cbf_get_value(chs, s);
+			System.out.println("get_val (" + status + ") = " + cbf.charPP_value(s));
+
+			status = cbf.cbf_find_column(chs, "array_id");
+			System.out.println("find_col (" + status + ")");
+		}
+		System.out.println("find_nrow (" + status + ")");
+
+		System.out.println("Dimensions: " + dims[0] + "x" + dims[1]);
+
+		// Attempt to load data
+		status = cbf.cbf_find_category(chs, "array_data");
+		System.out.println("find_cat (" + status + ")");
+		status = cbf.cbf_find_column(chs, "array_id");
+		System.out.println("find_col (" + status + ")");
+		status = cbf.cbf_find_row(chs, arrayid);
+		System.out.println("find_row (" + status + ")");
+		status = cbf.cbf_find_column(chs, "data");
+		System.out.println("find_col (" + status + ")");
+	}
+
+	/**
+	 * @param name
+	 * @return time in milliseconds
+	 */
+	public static int loadFile(String name) {
+		long start = -System.currentTimeMillis();
+		cbf_handle_struct chs = new cbf_handle_struct(name);
+		if (chs == null)
+			System.err.println("Could not open file or make handle structure");
+
+		int status = 0;
+
+		status = cbf.cbf_rewind_datablock(chs);
+		System.out.println("rewind_db (" + status + ")");
+
+		uintP mp = new uintP();
+
 		status = cbf.cbf_count_datablocks(chs, mp.cast());
 		System.out.println("count_dbs (" + status + ") = " + mp.value());
+
+		SWIGTYPE_p_p_char dbnamep = cbf.new_charPP();
+		status = cbf.cbf_datablock_name(chs, dbnamep);
+		System.out.println("db_name (" + status + ") = " + cbf.charPP_value(dbnamep));
+
+		int[] ntype = {0};
+		status = cbf.cbf_rewind_blockitem(chs, ntype);
+		System.out.println("rewind_bi (" + status + ") = " + ntype[0]);
+
+		status = cbf.cbf_find_category(chs, "diffrn_data_frame");
+		System.out.println("find_cat (" + status + ")");
+		if (status != 0) {
+			status = cbf.cbf_find_category(chs, "diffrn_frame_data");
+			System.out.println("find_cat (" + status + ")");
+		}
+
+		boolean fullheader = false;
+		int[] dims = new int[2];
+
+		if (status == 0) {
+			readheader(chs, dims);
+			fullheader = true;
+		} else {
+			System.out.println("Mini CBF!");
+			status = cbf.cbf_find_category(chs, "array_data");
+			System.out.println("find_cat (" + status + ")");
+			status = cbf.cbf_find_column(chs, "data");
+			System.out.println("find_col (" + status + ")");
+		}
+
+		int dsize = dims[0]*dims[1];
+
+		uintP cifcomp = new uintP();
+		intP bid = new intP(), els = new intP(), elu = new intP();
+		intP minel = new intP(), maxel = new intP(), isre = new intP();
+		sizetP elsize = new sizetP(), elnum = new sizetP();
+		sizetP dim1 = new sizetP(), dim2 = new sizetP(), dim3 = new sizetP(), pad = new sizetP();
+		SWIGTYPE_p_p_char byteorder = cbf.new_charPP();
+
+		status = cbf.cbf_get_arrayparameters_wdims(chs, cifcomp.cast(), bid.cast(), elsize.cast(),
+							els.cast(), elu.cast(), elnum.cast(),
+							minel.cast(), maxel.cast(), isre.cast(),
+							byteorder, dim1.cast(), dim2.cast(), dim3.cast(),
+							pad.cast());
+		System.out.println("get_aparams (" + status + ") = " + cifcomp.value() + ", "  + bid.value()
+					+ ", "  + elsize.value() + ", "  + els.value() + ", "  + elu.value() + ",");
+		System.out.println(" "  + elnum.value() + ", "  + minel.value() + ", "  + maxel.value()
+					+ ", "  + isre.value() + ", "  + cbf.charPP_value(byteorder) + ",");
+		System.out.println(" " + dim1.value() + ", "  + dim2.value() + ", "  + dim3.value()
+					+ ", "  + pad.value());
+		if (fullheader && dsize != elnum.value()) {
+			System.out.println("Mismatch of CBF binary data size: " + dsize + " != " + elnum.value());
+			return 0;
+		}
+		dsize = (int) elnum.value();
+		int elbytes = (int) elsize.value();
+
+		boolean isreal = (isre.value() == 1);
+		cifcomp.delete();
+		elu.delete(); minel.delete(); maxel.delete(); isre.delete();
+		elsize.delete(); elnum.delete();
+		dim1.delete(); dim2.delete(); dim3.delete(); pad.delete();
+
+		sizetP rsize = new sizetP();
+
+		long dstart = -System.currentTimeMillis();
+
+		if (isreal) {
+
+			if (elbytes != Float.SIZE/8 && elbytes != Double.SIZE/8) {
+				System.err.println("Element size, " + elbytes + ", not supported!\n");
+			} else {
+				ByteBuffer bdata = ByteBuffer.allocateDirect(elbytes*dsize).order(ByteOrder.nativeOrder());
+				status = cbf.cbf_get_realarray(chs, bid.cast(), bdata, elbytes, dsize, rsize.cast());
+				if (status == 0) {
+					System.out.println("get_realarray (" + status + ") " + rsize.value() + "/" + dsize);
+				
+					switch (elbytes) {
+					case Float.SIZE/8:
+						float[] farray = new float[dsize];
+						bdata.asFloatBuffer().get(farray);
+						System.out.println("Sample is " + farray[42]);
+						farray = null;
+						break;
+					case Double.SIZE/8:
+						double[] darray = new double[dsize];
+						bdata.asDoubleBuffer().get(darray);
+						System.out.println("Sample is " + darray[42]);
+						darray = null;
+						break;
+					}
+				}
+
+				bdata = null;
+			}
+
+		} else {
+			if (elbytes != Byte.SIZE/8 && elbytes != Short.SIZE/8 && elbytes != Integer.SIZE/8 && elbytes != Long.SIZE/8) {
+				System.err.println("Element size, " + elbytes + ", not supported!\n");
+			} else {
+				ByteBuffer bdata = ByteBuffer.allocateDirect(elbytes*dsize).order(ByteOrder.nativeOrder());
+				status = cbf.cbf_get_integerarray(chs, bid.cast(), bdata, elbytes, els.value(), dsize, rsize.cast());
+				if (status == 0) {
+					System.out.println("get_intarray (" + status + ") " + rsize.value() + "/" + dsize);
+
+					switch (elbytes) {
+					case Byte.SIZE/8:
+						byte[] barray = new byte[dsize];
+						bdata.get(barray);
+						System.out.println("Sample is " + barray[42]);
+						barray = null;
+						break;
+					case Short.SIZE/8:
+						short[] sarray = new short[dsize];
+						bdata.asShortBuffer().get(sarray);
+						System.out.println("Sample is " + sarray[42]);
+						sarray = null;
+						break;
+					case Integer.SIZE/8:
+						int[] iarray = new int[dsize];
+						bdata.asIntBuffer().get(iarray);
+						System.out.println("Sample is " + iarray[42]);
+						iarray = null;
+						break;
+					case Long.SIZE/8:
+						long[] larray = new long[dsize];
+						bdata.asLongBuffer().get(larray);
+						System.out.println("Sample is " + larray[42]);
+						larray = null;
+						break;
+					}
+
+					bdata = null;
+				}
+			}
+		}
+
+		rsize.delete();
+		bid.delete(); els.delete();
+		chs.delete();
+		long now = System.currentTimeMillis();
+		dstart += now;
+		start += now;
+		System.err.printf("Time: %dms (%dms)\n", start, dstart);
+		return (int) start;
 	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[]args) {
+		String name;
+		if (args.length < 1) {
+			name = "examples/template_pilatus6m_2463x2527.cbf";
+		} else {
+			name = args[0];
+		}
+		loadFile(name);
+
+		int n;
+		int t = 0;
+		for (n = 0; n < 10; n++)
+			t += loadFile(name);
+		System.err.printf("Ave: %dms\n", t/10);
+	}
+
 }
